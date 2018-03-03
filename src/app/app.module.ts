2516714bf474492f92d292b7b288a48efaaa2816
Jonathan Ziller <jonathan.ziller@gmail.com>
2018-03-03 16:57:03 +0100

    additional setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot((s: any, a: any) => s),
+    RouterModule.forRoot(appRoutes),
+    StoreModule.forRoot((s: any, _: any) => s),
     EffectsModule.forRoot([]),
   ],
   providers: [],
